c6a94cf7005e45fe57bd163f4ef8c15838002ad5
ccastroni <claudio.castroni@hotmail.cl>
2020-02-06 17:26:18 -0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,56 +1,71 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
-import { AlertController } from '@ionic/angular';
-import { Router, ActivatedRoute } from '@angular/router';
-import * as firebase from 'firebase';
-import { AboutPage } from '../about/about';
-
+import { AlertController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+import * as firebase from "firebase";
+import { AboutPage } from "../about/about";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   infos = [];
-  ref = firebase.database().ref('MenuTF/');
+  ref = firebase.database().ref("MenuTF/");
+  urlimagen = "";
 
   constructor(public navCtrl: NavController) {
-    this.ref.on('value', resp => {
-      console.log(resp)
+    this.ref.on("value", resp => {
+      // console.log(resp)
       this.infos = [];
       this.infos = snapshotToArray(resp);
-       console.log(this.infos)
+      // console.log(this.infos)
 
-       // let storageRef = firebase.storage().ref();
-    //var imageRef = storageRef.child('Menu').storage.app;
-    //console.log(imageRef)
+      // Points to the root reference
+      var storageRef = firebase.storage().ref();
+
+      // Points to 'images'
+      var imagesRef = storageRef.child("Menu");
+
+      // Points to 'images/space.jpg'
+      // Note that you can use variables to create child values
+      var fileName = "animated.png";
+      var spaceRef = imagesRef.child(fileName);
+
+      // File path is 'images/space.jpg'
+      var path = spaceRef.fullPath;
+
+      // File name is 'space.jpg'
+      var name = spaceRef.name;
+
+      // Points to 'images'
+      var imagesRef = spaceRef.parent;
+
+      this.urlimagen = path; //spaceRef.fullPath;
+      console.log(spaceRef);
+
     });
-    
-  }
-  
-   public ListaFiguras(item,linea) {
-     console.log(item)
-    this.navCtrl.push(AboutPage,{
-      item:item,
-      linea:linea
-    });
-   // alert('Download ' + item);
   }
 
+  public ListaFiguras(item, linea) {
+    console.log(item);
+    this.navCtrl.push(AboutPage, {
+      item: item,
+      linea: linea
+    });
+    // alert('Download ' + item);
+  }
 }
 
-
-
 export const snapshotToArray = snapshot => {
-    let returnArr = [];
+  let returnArr = [];
 
-    snapshot.forEach(childSnapshot => {
-      let item = childSnapshot.val();
-      item.key = childSnapshot.key;
-      returnArr.push(item);
-    });
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
 
-    return returnArr;
-  };
+  return returnArr;
+};
